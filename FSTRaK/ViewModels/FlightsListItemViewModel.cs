72accff8eb352ded59aa9c9193d2f598d21218a8
Oren Geva <o4oren@gmail.com>
2023-06-05 00:31:47 +0300

    Text block with runs etc

---
+++ FSTRaK/ViewModels/FlightsListItemViewModel.cs
@@ -0,0 +1,18 @@
+﻿using FSTRaK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSTRaK.ViewModels
+{
+    internal class FlightsListItemViewModel
+    {
+        private Flight _flight;
+        public Flight Flight { get { return _flight; } }
+        public FlightsListItemViewModel(Flight flight) {
+            _flight = flight;
+        }
+    }
+}
