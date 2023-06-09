ca01b3511bfe862767044e60ee094b4fb846bc56
Oren Geva <o4oren@gmail.com>
2023-06-10 02:30:58 +0300

    Fixed setup

--- FSTRaK/Properties/Settings.Designer.cs
+++ FSTRaK/Properties/Settings.Designer.cs
@@ -145,7 +145,7 @@ namespace FSTRaK.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool IsMinimizeToTray {
             get {
                 return ((bool)(this["IsMinimizeToTray"]));
