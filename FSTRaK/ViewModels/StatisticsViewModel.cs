a55644d98009354542fc96910269bd16426d1d5c
Oren Geva <o4oren@gmail.com>
2023-12-15 17:11:17 +0200

    Connect statistics to db

--- FSTRaK/ViewModels/StatisticsViewModel.cs
+++ FSTRaK/ViewModels/StatisticsViewModel.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FSTRaK.Models;
+using FSTRaK.Models.Entity;
 
 namespace FSTRaK.ViewModels
 {
     internal class StatisticsViewModel : BaseViewModel
     {
+        private int _totalNumberOfFlights;
+
+        public int TotalNumberOfFlights
+        {
+            get => _totalNumberOfFlights;
+            set
+            {
+                _totalNumberOfFlights = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public StatisticsViewModel()
+        {
+            // Flights = new ObservableCollection<Flight>();
+            using var logbookContext = new LogbookContext();
+            TotalNumberOfFlights = logbookContext.Flights.Count();
+
+        }
     }
 }
