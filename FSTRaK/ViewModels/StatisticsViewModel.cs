34c0ca127a57694ea330e8ab7a817002aa433d58
Oren Geva <o4oren@gmail.com>
2023-12-15 19:04:25 +0200

    Total and average distance and time

--- FSTRaK/ViewModels/StatisticsViewModel.cs
+++ FSTRaK/ViewModels/StatisticsViewModel.cs
@@ -5,6 +5,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
 using FSTRaK.Models;
 using FSTRaK.Models.Entity;
 
@@ -12,8 +13,10 @@ namespace FSTRaK.ViewModels
 {
     internal class StatisticsViewModel : BaseViewModel
     {
-        private int _totalNumberOfFlights;
+        private ObservableCollection<Flight> _flights;
 
+
+        private int _totalNumberOfFlights;
         public int TotalNumberOfFlights
         {
             get => _totalNumberOfFlights;
@@ -24,11 +27,78 @@ namespace FSTRaK.ViewModels
             }
         }
 
+
+
+        private string _totalFlightTime;
+        public string TotalFlightTime
+        {
+            get => _totalFlightTime;
+            set
+            {
+                _totalFlightTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _avgFlightTime;
+        public string AvgFlightTime
+        {
+            get => _avgFlightTime;
+            set
+            {
+                _avgFlightTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _totalFlightDistance;
+        public string TotalFlightDistance
+        {
+            get => _totalFlightDistance;
+            set
+            {
+                _totalFlightDistance = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _avgFlightDistance;
+        public string AvgFlightDistance
+        {
+            get => _avgFlightDistance;
+            set
+            {
+                _avgFlightDistance = value;
+                OnPropertyChanged();
+            }
+        }
+
+
         public StatisticsViewModel()
         {
-            // Flights = new ObservableCollection<Flight>();
+            UpdateStatistics();
+
+        }
+
+        private void UpdateStatistics()
+        {
             using var logbookContext = new LogbookContext();
-            TotalNumberOfFlights = logbookContext.Flights.Count();
+            var flights = logbookContext.Flights
+                .Select(f => f)
+                .OrderByDescending(f => f.Id)
+                .Include(f => f.Aircraft);
+            
+            TotalNumberOfFlights = flights.Count();
+            
+            var totalFlightMilis = flights.Sum(f => f.FlightTimeMilis);
+            var averageFlightMilis = flights.Average(f => f.FlightTimeMilis);
+            var totalFlightTimeTs = TimeSpan.FromTicks(totalFlightMilis);
+            var avgFlightTimeTs = TimeSpan.FromTicks((long)averageFlightMilis);
+            TotalFlightTime = $"{(int)totalFlightTimeTs.TotalHours}:{totalFlightTimeTs.Minutes}:{totalFlightTimeTs.Seconds}";
+            AvgFlightTime = $"{(int)avgFlightTimeTs.TotalHours}:{avgFlightTimeTs.Minutes}:{avgFlightTimeTs.Seconds}";
+
+            TotalFlightDistance = $"{flights.Sum(f => f.FlightDistanceNm).ToString("N1")}";
+            AvgFlightDistance = $"{flights.Average(f => f.FlightDistanceNm).ToString("N1")}";
 
         }
     }
