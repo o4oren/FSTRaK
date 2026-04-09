c4658c0d2627a18d2aed20daf15bc6cfa691b5f6
Oren Geva <o4oren@gmail.com>
2026-04-09 10:37:48 +0300

    build efb

--- msfs-addon/fstrak-efb-app/html_ui/efb_ui/efb_apps/FSTRaKApp/FSTRaKApp.js
+++ msfs-addon/fstrak-efb-app/html_ui/efb_ui/efb_apps/FSTRaKApp/FSTRaKApp.js
@@ -1,38 +1,49 @@
 (() => {
+  var __create = Object.create;
   var __defProp = Object.defineProperty;
-  var __getOwnPropSymbols = Object.getOwnPropertySymbols;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __getProtoOf = Object.getPrototypeOf;
   var __hasOwnProp = Object.prototype.hasOwnProperty;
-  var __propIsEnum = Object.prototype.propertyIsEnumerable;
-  var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
-  var __spreadValues = (a, b) => {
-    for (var prop in b || (b = {}))
-      if (__hasOwnProp.call(b, prop))
-        __defNormalProp(a, prop, b[prop]);
-    if (__getOwnPropSymbols)
-      for (var prop of __getOwnPropSymbols(b)) {
-        if (__propIsEnum.call(b, prop))
-          __defNormalProp(a, prop, b[prop]);
-      }
-    return a;
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
   };
-  var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
-    get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
-  }) : x)(function(x) {
-    if (typeof require !== "undefined") return require.apply(this, arguments);
-    throw Error('Dynamic require of "' + x + '" is not supported');
+  var __copyProps = (to, from, except, desc) => {
+    if (from && typeof from === "object" || typeof from === "function") {
+      for (let key of __getOwnPropNames(from))
+        if (!__hasOwnProp.call(to, key) && key !== except)
+          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+    }
+    return to;
+  };
+  var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+    // If the importer is in node compatibility mode or this is not an ESM
+    // file that has been converted to a CommonJS file using a Babel-
+    // compatible transform (i.e. "__esModule" has not been set), then set
+    // "default" to the CommonJS "module.exports" for node compatibility.
+    isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+    mod
+  ));
+
+  // global-externals:@efb/efb-api
+  var require_efb_api = __commonJS({
+    "global-externals:@efb/efb-api"(exports, module) {
+      module.exports = EFB_API;
+    }
   });
 
   // src/FSTRaKApp.tsx
-  var import_efb_api = __require("@efb/efb-api");
+  var import_efb_api = __toESM(require_efb_api());
   var FSTRaKAppView = class extends import_efb_api.AppView {
     render() {
-      return /* @__PURE__ */ React.createElement("div", { class: "fstrak-efb-container" }, /* @__PURE__ */ React.createElement(
-        "iframe",
-        {
+      return FSComponent.buildComponent(
+        "div",
+        { class: "fstrak-efb-container" },
+        FSComponent.buildComponent("iframe", {
           src: "http://127.0.0.1:8765/panel",
           class: "fstrak-efb-iframe"
-        }
-      ));
+        })
+      );
     }
   };
   var FSTRaKApp = class extends import_efb_api.App {
@@ -54,7 +65,7 @@
       );
     }
     render(props) {
-      return /* @__PURE__ */ React.createElement(FSTRaKAppView, __spreadValues({}, props));
+      return new FSTRaKAppView(props);
     }
   };
   import_efb_api.Efb.use(FSTRaKApp);
